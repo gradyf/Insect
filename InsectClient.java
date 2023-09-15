ed47fc00fed25f168d301e4b960790dc2836068b
Gray Forrester <gradyforrester3@gmail.com>
2023-09-14 21:48:27 -0400

    Continued to follow along with edX

---
+++ InsectClient.java
@@ -0,0 +1,9 @@
+public class InsectClient {
+    public static void main(String args[]) {
+        System.out.println(Insect.produceRandomFact());
+        Insect bug1 = new Insect(13, 31, 0);
+        //bug1.weight = -1;
+
+    }
+
+}
